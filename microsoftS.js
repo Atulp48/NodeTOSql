0d5a71efefe7238c1a5e155556fc446bc6dfd6be
atul <patelatul948@gmail.com>
2024-08-24 12:43:25 +0530

    second

--- microsoftS.js
+++ microsoftS.js
@@ -1,39 +1,39 @@
-// this is for sql server authentication
-
 const sql = require("mssql");
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const dbConfig = {
-  user: "user",
-  password: "root",
-  server: "ATUL",
-  database: "learning",
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  server: process.env.SERVER_NAME,
+  database: process.env.DATABASE_NAME,
   options: {
     encrypt: true,
     trustServerCertificate: true,
     trustedConnection: false,
     enableArithAbort: true,
-    instancename: "SQLEXPRESS",
+    instancename: process.env.INSTANCE_NAME,
   },
   port: 1433,
 };
 
 let jsonData = null;
-const table = "dashboard";
-const column = "id";
+const column = process.env.COLUMN_NAME;
 
 const fetchDataAndConvertToJson = async () => {
   try {
     const pool = await sql.connect(dbConfig);
 
     // const result = await connection.execute(`SELECT * FROM ${table} LIMIT 2`);
-    const result = await pool.request().query(`SELECT TOP 2 * FROM ${table}`);
+    const result = await pool
+      .request()
+      .query(`SELECT TOP 2 * FROM ${process.env.TABLE_NAME}`);
     // const result = await pool.request().query(`SELECT TOP 20 * FROM ${table} ORDER BY ${column} DESC`);
 
     jsonData = JSON.stringify(result.recordset, null, 2);
-    console.log("Data fetched and converted to JSON.");
-    console.log(jsonData);
+    console.log("Data fetched Successfully.");
+    // console.log(jsonData);
     await pool.close();
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -63,7 +63,7 @@ app.get("/getdata", (req, res) => {
   }
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.listen(process.env.PORT_NAME || 9000, () => {
+  console.log(`Server is running on port ${process.env.PORT_NAME || 9000}`);
   fetchDataAndConvertToJson();
 });
--- mysql.js
+++ mysql.js
@@ -1,17 +1,21 @@
 const mysql = require("mysql2/promise");
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
 
 const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "user",
-  database: "college",
+  host: process.env.HOST_NAME,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE_NAME,
 };
 
 let jsonData = null;
-const table = "Efficiencies";
-const column = "id";
+const table = process.env.TABLE_NAME;
+const column = process.env.COLUMN_NAME;
 const fetchDataAndConvertToJson = async () => {
   try {
     const connection = await mysql.createConnection(dbConfig);
@@ -22,6 +26,7 @@ const fetchDataAndConvertToJson = async () => {
     //     ORDER BY ${column} DESC
     //     LIMIT 20
     //   `);
+    // console.log(rows)
     jsonData = JSON.stringify(rows, null, 2);
     console.log("data comming");
     // console.log("JSON Data:", jsonData);
@@ -35,25 +40,21 @@ const startAutoFetch = () => {
   setInterval(fetchDataAndConvertToJson, 1000 * 10);
   //   setInterval(fetchDataAndConvertToJson, 1000*60*60*24);
 };
-const app = express();
-app.use(cors());
 app.get("/getdata", (req, res) => {
   if (jsonData) {
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "data fetches successfully",
-        data: JSON.parse(jsonData),
-      });
+    res.status(200).json({
+      success: true,
+      message: "data fetches successfully",
+      data: JSON.parse(jsonData),
+    });
   } else {
     res
-      .status(503)
+      .status(404)
       .json({ success: false, message: "Data is not yet available." });
   }
 });
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.listen(process.env.PORT_NAME || 9000, () => {
+  console.log(`Server is running on port ${process.env.PORT_NAME || 9000}`);
   //   startAutoFetch();
   fetchDataAndConvertToJson();
 });
